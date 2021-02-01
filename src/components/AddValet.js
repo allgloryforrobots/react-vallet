7290da631f1be138c43334d5a80a20161aa57a75
allgloryforrobots <75021483+allgloryforrobots@users.noreply.github.com>
2021-02-01 20:07:49 +0300

    first commit

---
+++ src/components/AddValet.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class AddVallet extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default AddVallet;
